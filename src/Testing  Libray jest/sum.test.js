95f1776789fdf392090d00aff611806d0e476f29
“Satyam@141026” <satyam@141026@gmail.com>
2022-07-11 10:01:21 +0530

    testing

---
+++ src/Testing  Libray jest/sum.test.js
@@ -0,0 +1,6 @@
+import sum from '../sum'
+it('when a and b is number tham result ',()=>{
+    const result=sum(3,"3");
+    expect(result).toBe(6);
+
+})
---
+++ src/sum.js
@@ -0,0 +1,5 @@
+function sum(a,b,c){
+    return parseInt(a,10)+parseInt(b,10)
+
+}
+export default sum
